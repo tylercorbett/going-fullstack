531e3d178e0e6e62670171fce19809aa717ca2aa
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-04 17:30:05 -0800

    all logic working

--- app/src/components/players/AddPlayer.vue
+++ app/src/components/players/AddPlayer.vue
@@ -1,10 +1,38 @@
 <template>
-    <h2>Add Player</h2>
+    <form @submit.prevent="handleSubmit">
+        <label> Name:
+            <input v-model="player.name" require>
+        </label>
+        <label> Number:
+            <input v-model="player.number" require>
+        </label>
+        <label> Is a starter?:
+            <input type="checkbox" v-model="player.isStarter" require>
+        </label>
+        <button>Add</button>
+    </form>
 </template>
 
 <script>
 export default {
-
+  props: {
+    onAdd: Function
+  },
+  data() {
+    return {
+      player: {
+        name: ''
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.onAdd(this.player)
+        .then(() => {
+          this.player = { name: '' };
+        });
+    }
+  }
 };
 </script>
 
