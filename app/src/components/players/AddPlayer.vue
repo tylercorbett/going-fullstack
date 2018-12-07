10209e9086b4d807516873672a79d0b8fb5b130b
Tyler Corbett <thetylercorbett@gmail.com>
2018-12-06 17:41:02 -0800

    positions showing up in ui. need to wire to db

--- app/src/components/players/AddPlayer.vue
+++ app/src/components/players/AddPlayer.vue
@@ -10,23 +10,50 @@
         <label > Is a starter?:
             <input type="checkbox" v-model="player.isStarter">
         </label>
+        <label>Position</label>
+        <select v-if="positions"
+          v-model="player.positionId"
+          required
+        >
+            <option value="-1" disabled>Select a position</option>
+            <option v-for="position in positions"
+              :key="position.id"
+              :value="position.id"
+            >
+              {{position.name}}  ({{position.type}})
+            </option>
+        </select>
         <button>Add</button>
     </form>
 </template>
 
 <script>
+import api from '../../services/api';
+
+function initPlayer() {
+  return {
+    name: '',
+    positionId: -1,
+    isStarter: false,
+    number: ''
+  };
+}
+
 export default {
   props: {
     onAdd: Function
   },
   data() {
     return {
-      player: {
-        name: '',
-        number: '',
-        isStarter: false
-      }
+      player: initPlayer(),
+      positions: null
     };
+  },
+  created() {
+    api.getPositions()
+      .then(positions => {
+        this.positions = positions;
+      });
   },
   methods: {
     handleSubmit() {
